11957a2f38c738dfe67501689513ce2f42df80e6
Johnmc104 <Johnmc104@qq.com>
2023-11-18 18:02:31 +0800

    init

---
+++ makefile
@@ -0,0 +1,27 @@
+#need java11
+
+antlr_version=4.8
+export CLASSPATH=/home/zhhe/work/sv_parser/antlr/antlr-${antlr_version}-complete.jar
+
+#antlr4=java -jar antlr/antlr-4.8-complete.jar
+antlr4=java -cp antlr/antlr-${antlr_version}-complete.jar org.antlr.v4.Tool
+
+systemverilog= \
+	systemverilog/SystemVerilogLexer.g4 \
+	systemverilog/SystemVerilogParser.g4 \
+	systemverilog/SystemVerilogPreParser.g4
+
+verilog= \
+	verilog/VerilogLexer.g4 \
+	verilog/VerilogParser.g4 \
+	verilog/VerilogPreParser.g4
+
+language=${systemverilog}
+
+python:pip3 install antlr4-python3-runtime==${antlr_version}
+
+test:
+	java org.antlr.v4.Tool
+
+run:
+	${antlr4} -no-listener -visitor -Dlanguage=Python3 ${language}
---
+++ run.py
@@ -0,0 +1,59 @@
+import string
+import sys
+from antlr4 import FileStream, CommonTokenStream
+from systemverilog.SystemVerilogLexer import SystemVerilogLexer
+from systemverilog.SystemVerilogParser import SystemVerilogParser
+from systemverilog.SystemVerilogParserVisitor import SystemVerilogParserVisitor
+from systemverilog.SystemVerilogPreParser import SystemVerilogPreParser
+from systemverilog.SystemVerilogPreParserVisitor import SystemVerilogPreParserVisitor
+
+
+class classVisitor(SystemVerilogParserVisitor):
+  def visitClass_declaration(self, ctx: SystemVerilogParser.Class_declarationContext):
+    rtn = ctx.class_identifier()
+    if rtn is not None: 
+      print('class:',rtn.getText())
+
+    rtn = ctx.class_extension() 
+    if rtn is not None: 
+      #print('class_extens:',rtn)
+      name = rtn.getText()
+      name = name.lstrip("extends")
+      print('class_extens:',name)
+
+    rtn = ctx.class_implementation() 
+    if rtn is not None: 
+      name = rtn.getText()
+      name = name.lstrip("implements")
+      print('class_implements:',name)
+
+    self.visitChildren(ctx)
+
+  def visitFunction_identifier(self, ctx: SystemVerilogParser.Function_identifierContext):
+    rtn = ctx.identifier()
+    if rtn is not None:
+      print('function:',rtn.getText())
+
+
+def main(argv):
+  input_stream = FileStream('test.sv')
+  lexer = SystemVerilogLexer(input_stream)
+  stream = CommonTokenStream(lexer)
+  parser = SystemVerilogParser(stream)
+  visitor = classVisitor()
+  context = parser.source_text()
+  ast = visitor.visitSource_text(context)
+
+if __name__ == "__main__":
+  main(sys.argv)
+
+#class classVisitor(SystemVerilogParserVisitor):
+#  def visitClass_declaration(self, ctx: SystemVerilogParser.Class_declarationContext):
+#    if len(rtn := ctx.class_identifier()):
+#      for tmp in rtn: print('class:',tmp.getText())
+#    if rtn := ctx.class_type():print('class_type:',rtn.getText())
+#    print("=========")
+#    self.visitChildren(ctx)
+#
+#  def visitFunction_identifier(self, ctx: SystemVerilogParser.Function_identifierContext):
+#    if rtn := ctx.identifier():print('function:',rtn.getText())
