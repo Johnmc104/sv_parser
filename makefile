3e2560570eacac9eb6daa868d35c3cb271a7f27f
Johnmc104 <Johnmc104@qq.com>
2024-01-23 11:19:05 +0800

    Update makefile and run_v.py to support both Verilog and SystemVerilog

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 #need java11
-
+language=verilog
 antlr_version=4.8
 export CLASSPATH=/home/zhhe/work/sv_parser/antlr/antlr-${antlr_version}-complete.jar
 
@@ -16,7 +16,13 @@ verilog= \
 	verilog/VerilogParser.g4 \
 	verilog/VerilogPreParser.g4
 
-language=${verilog}
+ifeq (${language}, systemverilog)
+language_lib=${systemverilog}
+python_cmd=run_sv.py test.sv
+else ifeq (${language}, verilog)
+language_lib=${verilog}
+python_cmd=run_v.py test.v
+endif
 
 python:pip3 install antlr4-python3-runtime==${antlr_version}
 
@@ -24,4 +30,7 @@ test:
 	java org.antlr.v4.Tool
 
 gen:
-	${antlr4} -no-listener -visitor -Dlanguage=Python3 ${language}
+	${antlr4} -no-listener -visitor -Dlanguage=Python3 ${language_lib}
+
+run:
+	@python3 ${python_cmd}
--- run_v.py
+++ run_v.py
@@ -1,5 +1,6 @@
 import string
 import sys
+import os
 import time
 from antlr4 import FileStream, CommonTokenStream
 
@@ -11,36 +12,70 @@ from verilog.VerilogPreParserVisitor import VerilogPreParserVisitor
 
 import re
 
-def print_matches(text):
-  matches = re.findall(r'(input|output|inout)(wire|reg)?(?:\[(\d+):(\d+)\])?(\w+)', text)
-  for match in matches:
-    dir, type, start, end, name = match
-    if start != '':
-      print(f'{dir} {type} {start}:{end} {name}')
-    else :
-      print(f'{dir} {type} {name}')
+class ParseResult:
+  def __init__(self):
+    self.ports = [] # list of dict
+
+  def set_ports(self, matches):
+    for i, match in enumerate(matches):
+      dir, type, start, end, name = match
+
+      result = {}
+      if dir    != '': result['dir'] = dir
+      if type   != '': result['type'] = type
+      if start  != '': 
+        result.update({'msb': start, 'lsb': end, 'name': name })
+      else : 
+        result['name'] = name
+
+      self.ports.append(result)
+      #print( "name: ", name, result)
+
+
+
+  def get_port_matches(self, text):
+    matches = re.findall(r'(input|output|inout)(wire|reg)?(?:\[(\d+):(\d+)\])?(\w+)', text)
+
+    if len(matches) > 0:
+      self.set_ports(matches)
+    else:
+      print("No results to update.")
+
 
 class moduleVisitor(VerilogParserVisitor):
+  def __init__(self):
+    self.results = ParseResult()
+
   def visitPort_declaration(self, ctx: VerilogParser.Port_declarationContext):
     rtn = ctx.inout_declaration()
     if rtn is not None: 
-      print('inout:',rtn.getText())
+      #print('inout:',rtn.getText())
+      self.results.get_port_matches(rtn.getText())
  
     rtn = ctx.input_declaration()
     if rtn is not None: 
-      print('input:',rtn.getText())
-      print_matches(rtn.getText())
+      #print('input:',rtn.getText())
+      self.results.get_port_matches(rtn.getText())
 
     rtn = ctx.output_declaration()
     if rtn is not None: 
-      print('output:',rtn.getText())
-
+      #print('output:',rtn.getText())
+      self.results.get_port_matches(rtn.getText())
 
 
 def main(argv):
   start = time.time()
 
-  input_stream = FileStream('test.v')
+  if len(sys.argv) > 1:
+      filename = sys.argv[1]
+  else:
+      filename = 'test.v'  # default filename
+
+  if not os.path.exists(filename):
+      print(f"File {filename} does not exist.")
+      sys.exit(1)
+
+  input_stream = FileStream(filename)
   lexer = VerilogLexer(input_stream)
   stream = CommonTokenStream(lexer)
   parser = VerilogParser(stream)
@@ -48,6 +83,10 @@ def main(argv):
   visitor = moduleVisitor()
   context = parser.source_text()
   ast = visitor.visitSource_text(context)
+
+  #print("=====================================")
+  #for port in visitor.results.ports:
+  #  print("Signal: ", port)
 
   print(f"Elapsed time: {time.time() - start} s")
 
